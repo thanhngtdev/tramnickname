f65340a34ac4a63b4dbd492fb37942b4b7647d93
PhanTri-techmaster <65991362+PhanTri-techmaster@users.noreply.github.com>
2021-02-17 18:48:55 +0700

    fix bug 14

--- Source/web/src/component/homepage/BannerTop.js
+++ Source/web/src/component/homepage/BannerTop.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { headerActionType, homeActionType } from '../../actions/actionTypes';
+import PathRoute from '../../common/PathRoute';
 
 function BannerTop() {
     const dispatch = useDispatch();
@@ -25,31 +26,56 @@ function BannerTop() {
                 <h1>We make football fun for every child</h1>
                 <div className="box-text">
                     <p>{message}</p>
-                    <div className="box-pin">
-                        <span className="text-form ">
-                            Find your nearest academy:
-                        </span>
-                        <div className="box">
-                            <input
-                                type="text"
-                                className="input-text"
-                                placeholder="TW7 5BB"
-                                onChange={(evt) =>
-                                    setTextSearch(evt.target.value)
-                                }
-                            />
-                            <button
-                                className="btn-pin"
-                                onClick={() =>
-                                    dispatch({
-                                        type: headerActionType.CHANGE_LOCATION,
-                                        data: textSearch,
-                                    })
-                                }>
-                                {defaultAcademy ? 'Book' : 'Find'}
-                            </button>
+                    {defaultAcademy.ms_id ? (
+                        <div
+                            className="box-button"
+                            style={{
+                                textAlign: 'center',
+                                padding: '20px 0px',
+                                borderRadius:"10px",
+                                backgroundColor:"#EE7925",
+                            }}>
+                            {' '}
+                            <a
+                                style={{ color: 'white' }}
+                                className="btn-book-free-session"
+                                href={PathRoute.BookTrialTraining}>
+                                {' '}
+                                BOOK A{' '}
+                                {defaultAcademy && defaultAcademy.ms_trial === 1
+                                    ? 'TRIAL'
+                                    : 'FREE'}{' '}
+                                SESSION{' '}
+                            </a>{' '}
                         </div>
-                    </div>
+                    ) : (
+                        <div className="box-pin">
+                            <span className="text-form ">
+                                Find your nearest academy:
+                            </span>
+                            <div className="box">
+                                <input
+                                    type="text"
+                                    className="input-text"
+                                    placeholder="TW7 5BB"
+                                    onChange={(evt) =>
+                                        setTextSearch(evt.target.value)
+                                    }
+                                />
+                                <button
+                                    className="btn-pin"
+                                    onClick={() =>
+                                        dispatch({
+                                            type:
+                                                headerActionType.CHANGE_LOCATION,
+                                            data: textSearch,
+                                        })
+                                    }>
+                                    {defaultAcademy ? 'Book' : 'Find'}
+                                </button>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
