8e0a7b7ddcba6dec6175ffa8b494282e8a29ee02
PhanTri-techmaster <65991362+PhanTri-techmaster@users.noreply.github.com>
2021-02-25 13:51:55 +0700

    fix bug 47,50,56

--- Source/web/src/component/booking/BookTrialTraining3.js
+++ Source/web/src/component/booking/BookTrialTraining3.js
@@ -59,6 +59,12 @@ function BookTrialTraining3(props) {
         window.scrollTo(0, 0);
     }, []);
 
+    useEffect(() => {
+        if(props.success === 1){
+            console.log("deptrai");
+        }
+    }, [props.success]);
+
     const siteReducer = useSelector((state) => state.siteReducer);
     useEffect(() => {
         if (siteReducer.type) {
