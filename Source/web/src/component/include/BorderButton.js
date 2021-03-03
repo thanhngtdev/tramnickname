9d49b2bb9b5955fe9d845f3a27553b9a833eb5fa
huyleemf <hoangduchuy03121999@gmail.com>
2021-03-03 16:40:20 +0700

    search by postcode - location page

--- Source/web/src/component/include/BorderButton.js
+++ Source/web/src/component/include/BorderButton.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react';
 
 export default function BorderButton(props) {
+    // useEffect(() => {
+    //     console.log(props.disabled, 'aa');
+    // }, [props.disabled]);
+
     return (
         <button
-            // disabled={true}
+            disabled={props.disabled}
             className={`border-btn ${props.disabled ? 'btn-disabled' : ''}`}
             onClick={() => {
                 if (props.onClick) props.onClick();
