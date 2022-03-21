6cd07c6e892d52ccecc5442a8be093b3b9605328
Huyleemf <hoangduchuy03121999@gmail.com>
2022-03-21 11:21:34 +0700

    fix slide mobile

--- Source/web/src/components/Camp/InstaBox.js
+++ Source/web/src/components/Camp/InstaBox.js
@@ -1,55 +1,104 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 // import 'slick-carousel/slick/slick.css';
 import Slider from 'react-slick';
 import Utils from 'src/common/Utils';
 import useGetWidth from 'src/hooks/useGetWidth';
 
 function InstaBox(props) {
-    let settings = {
-        lazyLoad: 'ondemand',
-        infinite: true,
-        autoplaySpeed: 2500,
-        autoplay: true,
-        speed: 1000,
-    };
     const { instaFeed } = props || {};
     const isMobile = useGetWidth() <= 768;
-    let numOfSlide = [];
+    const [numOfSlide, setNumOfSlide] = useState([]);
     let lstImage = instaFeed ? instaFeed.cfg_value : [];
 
-    if (isMobile) {
-        if (lstImage && lstImage.length > 0) {
+    let settings = {
+        // lazyLoad: 'ondemand',
+        infinite: true,
+        autoplaySpeed: 2500,
+        autoplay: !isMobile,
+        speed: 1000,
+    };
+
+    useEffect(() => {
+        if (instaFeed.cfg_value) {
             let i;
             let slideImage = [];
-            for (i = 0; i < lstImage.length; i++) {
-                slideImage.push(lstImage[i]);
-            }
-            numOfSlide.push(slideImage);
+            let newList = [];
+            let marker = 0;
 
-            settings = { ...settings, autoplay: false };
-        }
-    } else {
-        if (lstImage && lstImage.length > 0) {
-            let i;
-            for (i = 0; i < lstImage.length / 6; i++) {
-                let slideImage = [];
-                slideImage.push(-1);
-                if (lstImage.length > 6 * i) slideImage.push(lstImage[6 * i]);
-                slideImage.push(-1);
-                if (lstImage.length > 6 * i + 1)
-                    slideImage.push(lstImage[6 * i + 1]);
-                if (lstImage.length > 6 * i + 2)
-                    slideImage.push(lstImage[6 * i + 2]);
-                if (lstImage.length > 6 * i + 3)
-                    slideImage.push(lstImage[6 * i + 3]);
-                if (lstImage.length > 6 * i + 4)
-                    slideImage.push(lstImage[6 * i + 4]);
-                if (lstImage.length > 6 * i + 5)
-                    slideImage.push(lstImage[6 * i + 5]);
-                numOfSlide.push(slideImage);
+            if (isMobile) {
+                for (i = 0; i < lstImage.length; i++) {
+                    if (marker < 6 && i === lstImage.length - 1) {
+                        newList.push(slideImage);
+                    }
+
+                    if (marker === 6) {
+                        newList.push(slideImage);
+                        marker = 0;
+                        slideImage = [];
+                    }
+
+                    if (i % 2 === 0) slideImage.push(-1);
+                    slideImage.push(lstImage[i]);
+                    marker++;
+                }
+            } else {
+                for (i = 0; i < lstImage.length / 6; i++) {
+                    let slideImage = [];
+                    slideImage.push(-1);
+                    if (lstImage.length > 6 * i)
+                        slideImage.push(lstImage[6 * i]);
+                    slideImage.push(-1);
+                    if (lstImage.length > 6 * i + 1)
+                        slideImage.push(lstImage[6 * i + 1]);
+                    if (lstImage.length > 6 * i + 2)
+                        slideImage.push(lstImage[6 * i + 2]);
+                    if (lstImage.length > 6 * i + 3)
+                        slideImage.push(lstImage[6 * i + 3]);
+                    if (lstImage.length > 6 * i + 4)
+                        slideImage.push(lstImage[6 * i + 4]);
+                    if (lstImage.length > 6 * i + 5)
+                        slideImage.push(lstImage[6 * i + 5]);
+                    newList.push(slideImage);
+                }
             }
+
+            setNumOfSlide(newList);
         }
-    }
+    }, [isMobile]);
+
+    // if (isMobile) {
+    //     if (lstImage && lstImage.length > 0) {
+    //         let i;
+    //         let slideImage = [];
+    //         for (i = 0; i < lstImage.length; i++) {
+    //             slideImage.push(lstImage[i]);
+    //         }
+    //         numOfSlide.push(slideImage);
+
+    //         settings = { ...settings, autoplay: false };
+    //     }
+    // } else {
+    //     if (lstImage && lstImage.length > 0) {
+    //         let i;
+    //         for (i = 0; i < lstImage.length / 6; i++) {
+    //             let slideImage = [];
+    //             slideImage.push(-1);
+    //             if (lstImage.length > 6 * i) slideImage.push(lstImage[6 * i]);
+    //             slideImage.push(-1);
+    //             if (lstImage.length > 6 * i + 1)
+    //                 slideImage.push(lstImage[6 * i + 1]);
+    //             if (lstImage.length > 6 * i + 2)
+    //                 slideImage.push(lstImage[6 * i + 2]);
+    //             if (lstImage.length > 6 * i + 3)
+    //                 slideImage.push(lstImage[6 * i + 3]);
+    //             if (lstImage.length > 6 * i + 4)
+    //                 slideImage.push(lstImage[6 * i + 4]);
+    //             if (lstImage.length > 6 * i + 5)
+    //                 slideImage.push(lstImage[6 * i + 5]);
+    //             numOfSlide.push(slideImage);
+    //         }
+    //     }
+    // }
 
     return (
         <Fragment>
@@ -85,15 +134,6 @@ function InstaBox(props) {
                                                             item.image,
                                                         )}
                                                     />
-                                                    {/* <div className="bg">
-                                                        <h3 className="title">
-                                                            {item.title &&
-                                                                `${
-                                                                    '@' +
-                                                                    item.title
-                                                                }`}
-                                                        </h3>
-                                                    </div> */}
                                                 </div>
                                             );
                                         })}
--- Source/web/src/components/HomePage/InstaBox.js
+++ Source/web/src/components/HomePage/InstaBox.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 // import 'slick-carousel/slick/slick.css';
 import Slider from 'react-slick';
 import Utils from 'src/common/Utils';
@@ -6,51 +6,65 @@ import useGetWidth from 'src/hooks/useGetWidth';
 
 function InstaBox(props) {
     const { instaFeed } = props || {};
+    const [numOfSlide, setNumOfSlide] = useState([]);
     const isMobile = useGetWidth() <= 768;
-    let numOfSlide = [];
     let lstImage = instaFeed ? instaFeed.cfg_value : [];
 
     let settings = {
-        lazyLoad: 'ondemand',
+        // lazyLoad: 'ondemand',
         infinite: true,
         speed: 1000,
         autoplaySpeed: 2500,
-        autoplay: true,
+        autoplay: !isMobile,
     };
 
-    if (isMobile) {
-        if (lstImage && lstImage.length > 0) {
+    useEffect(() => {
+        if (instaFeed.cfg_value) {
             let i;
             let slideImage = [];
-            for (i = 0; i < lstImage.length; i++) {
-                slideImage.push(lstImage[i]);
-            }
-            numOfSlide.push(slideImage);
+            let newList = [];
+            let marker = 0;
 
-            settings = { ...settings, autoplay: false };
-        }
-    } else {
-        if (lstImage && lstImage.length > 0) {
-            let i;
-            for (i = 0; i < lstImage.length / 6; i++) {
-                let slideImage = [];
-                slideImage.push(-1);
-                if (lstImage.length > 6 * i) slideImage.push(lstImage[6 * i]);
-                slideImage.push(-1);
-                if (lstImage.length > 6 * i + 1)
-                    slideImage.push(lstImage[6 * i + 1]);
-                if (lstImage.length > 6 * i + 2)
-                    slideImage.push(lstImage[6 * i + 2]);
-                if (lstImage.length > 6 * i + 3)
-                    slideImage.push(lstImage[6 * i + 3]);
-                if (lstImage.length > 6 * i + 4)
-                    slideImage.push(lstImage[6 * i + 4]);
-                if (lstImage.length > 6 * i + 5)
-                    slideImage.push(lstImage[6 * i + 5]);
-                numOfSlide.push(slideImage);
+            if (isMobile) {
+                for (i = 0; i < lstImage.length; i++) {
+                    if (marker < 6 && i === lstImage.length - 1) {
+                        newList.push(slideImage);
+                    }
+
+                    if (marker === 6) {
+                        newList.push(slideImage);
+                        marker = 0;
+                        slideImage = [];
+                    }
+
+                    if (i % 2 === 0) slideImage.push(-1);
+                    slideImage.push(lstImage[i]);
+                    marker++;
+                }
+            } else {
+                for (i = 0; i < lstImage.length / 6; i++) {
+                    let slideImage = [];
+                    slideImage.push(-1);
+                    if (lstImage.length > 6 * i)
+                        slideImage.push(lstImage[6 * i]);
+                    slideImage.push(-1);
+                    if (lstImage.length > 6 * i + 1)
+                        slideImage.push(lstImage[6 * i + 1]);
+                    if (lstImage.length > 6 * i + 2)
+                        slideImage.push(lstImage[6 * i + 2]);
+                    if (lstImage.length > 6 * i + 3)
+                        slideImage.push(lstImage[6 * i + 3]);
+                    if (lstImage.length > 6 * i + 4)
+                        slideImage.push(lstImage[6 * i + 4]);
+                    if (lstImage.length > 6 * i + 5)
+                        slideImage.push(lstImage[6 * i + 5]);
+                    newList.push(slideImage);
+                }
             }
+
+            setNumOfSlide(newList);
         }
-    }
+    }, [isMobile]);
 
     return (
         <Fragment>
@@ -61,52 +75,54 @@ function InstaBox(props) {
                     <div className="box-slide">
                         <div className="row ">
                             <Slider className="list lazy" {...settings}>
-                                {numOfSlide.map((element, index) => (
-                                    <div className="item" key={index}>
-                                        {element.map((item, idx) => {
-                                            if (item === -1) {
-                                                return (
-                                                    <div
-                                                        key={idx}
-                                                        className="photo"
-                                                    />
-                                                );
-                                            }
-                                            return (
-                                                <div
-                                                    key={idx}
-                                                    className="photo"
-                                                    style={{
-                                                        cursor: 'pointer',
-                                                    }}
-                                                    onClick={() => {
-                                                        window.open(
-                                                            'https://www.instagram.com/' +
-                                                                item.title,
-                                                            '_blank',
+                                {numOfSlide &&
+                                    numOfSlide.map((element, index) => (
+                                        <div className="item" key={index}>
+                                            {element &&
+                                                element.map((item, idx) => {
+                                                    if (item === -1) {
+                                                        return (
+                                                            <div
+                                                                key={idx}
+                                                                className="photo"
+                                                            />
                                                         );
-                                                    }}>
-                                                    <img
-                                                        loading="lazy"
-                                                        alt=""
-                                                        src={Utils.getThumb(
-                                                            item.image,
-                                                        )}
-                                                    />
-                                                    <div className="bg">
-                                                        <h3 className="title">
-                                                            {item.title &&
-                                                                `${
-                                                                    '@' +
-                                                                    item.title
-                                                                }`}
-                                                        </h3>
-                                                    </div>
-                                                </div>
-                                            );
-                                        })}
-                                    </div>
-                                ))}
+                                                    }
+                                                    return (
+                                                        <div
+                                                            key={idx}
+                                                            className="photo"
+                                                            style={{
+                                                                cursor: 'pointer',
+                                                            }}
+                                                            onClick={() => {
+                                                                window.open(
+                                                                    'https://www.instagram.com/' +
+                                                                        item.title,
+                                                                    '_blank',
+                                                                );
+                                                            }}>
+                                                            <img
+                                                                loading="lazy"
+                                                                alt=""
+                                                                src={Utils.getThumb(
+                                                                    item.image,
+                                                                )}
+                                                            />
+                                                            <div className="bg">
+                                                                <h3 className="title">
+                                                                    {item.title &&
+                                                                        `${
+                                                                            '@' +
+                                                                            item.title
+                                                                        }`}
+                                                                </h3>
+                                                            </div>
+                                                        </div>
+                                                    );
+                                                })}
+                                        </div>
+                                    ))}
                             </Slider>
                         </div>
                     </div>
