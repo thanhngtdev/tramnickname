f52176d8f99b9367d2ca27a4d12c4caaec0ba223
hung1e <leminhh@gmail.com>
2022-05-12 23:43:03 +0700

    correct

--- Source/web/src/pages/[franchise]/news/index.js
+++ Source/web/src/pages/[franchise]/news/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import siteService from 'src/services/siteService';
 import dynamic from 'next/dynamic';
 const CategoryNews = dynamic(() => import('src/components/CategoryNews'));
-const DefaultLayout = dynamic(() => import('src/layout/DefaultLayout'));
+//const DefaultLayout = dynamic(() => import('src/layout/DefaultLayout'));
 const SiteNews = (props) => {
     console.log(props, 'props');
    
@@ -18,11 +18,11 @@ const SiteNews = (props) => {
 export async function getServerSideProps(ctx) {
     const listRes = await siteService.getListSite();
     const listSite = listRes.data.data.lstSite;
-    const siteDetails = await Utils.getDetailMicrosite(
-        context.params.franchise,
-        18,
-        'news',
-    );
+    // const siteDetails = await Utils.getDetailMicrosite(
+    //     context.params.franchise,
+    //     18,
+    //     'news',
+    // );
     const item = listSite.find((item) => ctx.query.franchise === item.ms_alias);
     if (isEmpty(item)) {
         return { notFound: true };
