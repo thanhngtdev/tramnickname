011b166c69281c9a9ee6e357d91cf87ca7c5e77d
hung1e <leminhh@gmail.com>
2022-05-13 00:07:16 +0700

    fix

--- Source/web/src/pages/[franchise]/news/index.js
+++ Source/web/src/pages/[franchise]/news/index.js
@@ -3,29 +3,29 @@ import React from 'react';
 import siteService from 'src/services/siteService';
 import dynamic from 'next/dynamic';
 const CategoryNews = dynamic(() => import('src/components/CategoryNews'));
-const DefaultLayout = dynamic(() => import('src/layout/DefaultLayout'));
+//const DefaultLayout = dynamic(() => import('src/layout/DefaultLayout'));
 const SiteNews = (props) => {
     console.log(props, 'props');
    
 
     return (
-        <DefaultLayout seo={detailSite.data?.seoMetaFranchise || {}}>
+       // <DefaultLayout seo={detailSite.data?.seoMetaFranchise || {}}>
     <CategoryNews listSite={props.listSite} data={props.data} />
-    </DefaultLayout>
+   // </DefaultLayout>
     );
 };
 
-export async function getServerSideDetailedProps(context) {
-    const detailSite = await Utils.getDetailMicrosite(
-        context.params.franchise,
-        14,
-        'news',
-    );
+// export async function getServerSideDetailedProps(context) {
+//     const detailSite = await Utils.getDetailMicrosite(
+//         context.params.franchise,
+//         14,
+//         'news',
+//     );
 
-    if (isEmpty(detailSite.data)) return { notFound: true };
+//     if (isEmpty(detailSite.data)) return { notFound: true };
 
-    return { detailSite };
-}
+//     return { detailSite };
+// }
 
 export async function getServerSideProps(ctx) {
     const listRes = await siteService.getListSite();
