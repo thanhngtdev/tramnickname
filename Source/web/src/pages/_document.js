9105960ae59960792c386bd431ef06222a5f3853
hung1e <leminhh@gmail.com>
2022-05-17 01:57:16 +0700

    google tag manager

--- Source/web/src/pages/_document.js
+++ Source/web/src/pages/_document.js
@@ -1,5 +1,7 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
+import Script from 'next/script'
+
 
 export default class MyDocument extends Document {
     render() {
@@ -21,37 +23,17 @@ export default class MyDocument extends Document {
                         async></script> */}
                         {/* <!-- Google Tag Manager --> */}
 
-                        {/* <Script
-                            id="google-analytics"
-                            src="https://www.googletagmanager.com/gtag/js?id=GTM-PMMW2QG"
-                            onLoad={() => {
-                                (function (w, d, s, l, i) {
-                                    w[l] = w[l] || [];
-                                    w[l].push({
-                                        'gtm.start': new Date().getTime(),
-                                        event: 'gtm.js',
-                                    });
-                                    var f = d.getElementsByTagName(s)[0],
-                                        j = d.createElement(s),
-                                        dl = l != 'dataLayer' ? '&l=' + l : '';
-                                    j.async = true;
-                                    j.src =
-                                        'https://www.googletagmanager.com/gtm.js?id=' +
-                                        i +
-                                        dl;
-                                    f.parentNode.insertBefore(j, f);
-                                })(window, document, 'script', 'dataLayer', 'GTM-PMMW2QG');
-                            }}
-                        /> */}
-                        {/* <!-- End Google Tag Manager --> */}
+                        <Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-PMMW2QG');`}}></Script>
                 </Head>
                 <body>
                     <Main />
                     <NextScript />
-                    {/* <!-- Google Tag Manager (noscript) --> */}
-                    {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PMMW2QG"
-                    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> */}
-                    {/* <!-- End Google Tag Manager (noscript) --> */}
+                    <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PMMW2QG"
+                    height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
                 </body>
             </Html>
         );
