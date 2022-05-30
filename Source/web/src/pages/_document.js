4f87f19258bb608e39718ecefc583aca885bd23f
huyleemf <hoangduchuy03121999@gmail.com>
2022-05-30 11:06:52 +0700

    add hotjar script

--- Source/web/src/pages/_document.js
+++ Source/web/src/pages/_document.js
@@ -4,6 +4,7 @@ import React from 'react';
 
 export default class MyDocument extends Document {
     render() {
+        // const HOTJAR_ID = process.env.HOTJAR_ID;
         return (
             <Html lang="en">
                 <Head>
@@ -14,6 +15,19 @@ export default class MyDocument extends Document {
                         rel="stylesheet"
                         href="/static-file/css/font.css"
                         as="style"
+                    />
+
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `(function(h,o,t,j,a,r){
+                        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                        h._hjSettings={hjid:2994035,hjsv:6};
+                        a=o.getElementsByTagName('head')[0];
+                        r=o.createElement('script');r.async=1;
+                        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                        a.appendChild(r);
+                    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`,
+                        }}
                     />
 
                     {/* <script
